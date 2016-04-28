ad8ca4e9a6f3c0526cff9a3f7363de369a0c9856
Tayler Phillips <tphillips@infotech.com>
2016-04-28 09:52:13 -0400

    Remove anything to do with book me in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def page_title(text = nil)
     content_for :title do
-      "BookMeIn#{': ' + text if text}"
+      "BaseApp#{': ' + text if text}"
     end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_options = { from: 'no-reply@bookmein.com' }
+  config.action_mailer.default_options = { from: 'no-reply@BaseApp.com' }
   config.action_mailer.delivery_method = :letter_opener
 
   # in order to use gmail
